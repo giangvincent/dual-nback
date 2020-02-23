35dbb1c59e12798a50387f745b587d41762b1900
Vincent Giang <giang.dlinh92@gmail.com>
2020-02-23 18:05:30 +0700

    add gameover and setting

--- src/router.js
+++ src/router.js
@@ -5,12 +5,28 @@ import Home from './views/Home.vue'
 Vue.use(Router)
 
 export default new Router({
+  mode: 'history',
   routes: [
     {
       path: '/',
       name: 'home',
       component: Home
     },
+    {
+      path: '/play',
+      name: 'play',
+      component: () => import('./views/GamePlay.vue')
+    },
+    {
+      path: '/game-over',
+      name: 'game-over',
+      component: () => import('./views/GameOver.vue')
+    },
+    {
+      path: '/setting',
+      name: 'setting',
+      component: () => import('./views/Setting.vue')
+    }
     /* {
       path: '/about',
       name: 'about',
---
+++ src/views/GameOver.vue
@@ -0,0 +1,38 @@
+<template>
+    <div id="game-over">
+        <div class="game-indicator">
+            <div class="columns is-mobile">
+                <div class="column is-half">
+                    n = 1
+                </div>
+                <div class="column is-half">
+                    300 light
+                </div>
+            </div>
+        </div><!-- indicator -->
+        <div class="summary">
+            <div class="columns avatar">
+                <img src="" alt="avatar">
+            </div>
+            <div class="columns progress">
+                <div class="score">
+                    69
+                </div>
+                <div class="nback">
+                    n = 2
+                </div>
+            </div>
+        </div><!-- summary -->
+        
+    </div>
+</template>
+
+<script>
+export default {
+    name: "game-over"
+}
+</script>
+
+<style lang="scss">
+@import url("../themes/GameOver.scss");
+</style>
