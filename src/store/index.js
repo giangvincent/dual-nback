b738be97c1914d140e9d283975276dcb8df309d4
Vincent Giang <giang.dlinh92@gmail.com>
2020-03-01 10:53:27 +0700

    update store: split main store into files

--- src/store/index.js
+++ src/store/index.js
@@ -2,10 +2,16 @@ import Vue from 'vue'
 import Vuex from 'vuex'
 import gamePlay from "./gamePlay"
 import user from "./user"
+import state from "./state"
+import actions from "./actions"
+import mutations from "./mutations"
 
 Vue.use(Vuex)
 
 export default new Vuex.Store({
+    state: state,
+    mutations: mutations,
+    actions: actions,
     modules: {
         game: gamePlay,
         user: user
