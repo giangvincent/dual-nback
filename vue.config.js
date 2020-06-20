0a40f485a27ecfba1f869cd830d92b3f78eada63
Vincent Giang <giang.dlinh92@gmail.com>
2020-06-20 10:57:49 +0700

    change vue build

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,9 @@
+const PurgecssPlugin = require("purgecss-webpack-plugin");
+const glob = require("glob-all");
+const path = require("path");
+
 module.exports = {
+    baseUrl: './',
     pluginOptions: {
         i18n: {
             locale: 'en',
@@ -10,6 +15,21 @@ module.exports = {
             https: true,
             port: 8000
         },
-        productionSourceMap: false
+        productionSourceMap: false,
+        configureWebpack: {
+            // optimization: {
+            //   splitChunks: 'all'
+            // },
+            plugins: [
+                new PurgecssPlugin({
+                    paths: glob.sync([
+                        // path.join(__dirname, './src/index.html'),
+                        path.join(__dirname, "./**/*.vue"),
+                        path.join(__dirname, "./src/**/*.js")
+                    ])
+                })
+                // new BundleAnalyzerPlugin()
+            ]
+        }
     }
 }
