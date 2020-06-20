0fc3de2b9507edd6d52db5ffd89ad79fc28d40bd
Vincent Giang <giang.dlinh92@gmail.com>
2020-06-20 11:06:03 +0700

    update packages

--- vue.config.js
+++ vue.config.js
@@ -15,21 +15,6 @@ module.exports = {
             https: true,
             port: 8000
         },
-        productionSourceMap: false,
-        configureWebpack: {
-            // optimization: {
-            //   splitChunks: 'all'
-            // },
-            plugins: [
-                new PurgecssPlugin({
-                    paths: glob.sync([
-                        // path.join(__dirname, './src/index.html'),
-                        path.join(__dirname, "./**/*.vue"),
-                        path.join(__dirname, "./src/**/*.js")
-                    ])
-                })
-                // new BundleAnalyzerPlugin()
-            ]
-        }
+        productionSourceMap: false
     }
 }
