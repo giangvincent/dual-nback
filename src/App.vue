c8c7fed6a9990cec28d7512deb1c7c0e45628123
mfvodka107 <giang.dlinh92@gmail.com>
2021-01-25 10:30:30 +0700

    fix load font to game

--- src/App.vue
+++ src/App.vue
@@ -1,35 +1,58 @@
 <template>
-  <div id="app" class="container">
-    <router-view/>
+  <div id="app">
+    <router-view />
   </div>
 </template>
 
 <script>
 /* eslint-disable no-console */
 /* eslint-disable no-undef */
-import { mapState } from 'vuex'
+import { mapState } from "vuex";
 
 export default {
   name: "main-app",
   data() {
-    return {}
+    return {};
   },
   computed: {
     ...mapState({
-      musicSetting: state => state.musicSetting,
-      homeMusic: state => state.music_home
-    })
+      musicSetting: (state) => state.musicSetting,
+      homeMusic: (state) => state.music_home,
+    }),
   },
-  watch: {
-  },
+  watch: {},
   mounted() {
     this.homeMusic.play();
     this.homeMusic.mute(!this.musicSetting);
-    this.homeMusic.volume(0.5)
-  }
-}
+    this.homeMusic.volume(0.5);
+  },
+};
 </script>
 
 <style lang="scss">
-@import url("./themes/index.scss");
+@import "./themes/index.scss";
+
+@font-face {
+  font-family: "Baloo";
+  font-style: normal;
+  font-weight: 700;
+  font-display: swap;
+  src: url("~@/assets/fonts/Baloo2-SemiBold.ttf") format("truetype");
+}
+
+@font-face {
+  font-family: "Baloo";
+  font-style: normal;
+  font-weight: 500;
+  font-display: swap;
+  src: url("~@/assets/fonts/Baloo2-Medium.ttf") format("truetype");
+}
+
+@font-face {
+  font-family: "Baloo";
+  font-style: normal;
+  font-weight: normal;
+  font-display: swap;
+  src: url("~@/assets/fonts/Baloo2-Regular.ttf") format("truetype");
+}
 </style>
--- src/main.js
+++ src/main.js
@@ -1,34 +1,33 @@
 /* eslint-disable no-console */
 /* eslint-disable no-undef */
-import Vue from 'vue'
-import App from './App.vue'
-import router from './router'
-import store from './store/'
-import i18n from './i18n'
+import Vue from "vue";
+import App from "./App.vue";
+import router from "./router";
+import store from "./store/";
+import i18n from "./i18n";
+import "@/assets/css/tailwind.css";
 
-Vue.config.productionTip = false
-Vue.config.errorHandler = function(err, vm, info) {
+Vue.config.productionTip = false;
+Vue.config.errorHandler = function (err, vm, info) {
   console.error("[Global Error Handler]: Error in " + info + ": " + err);
   // eslint-disable-next-line
   // ga && ga("send", "event", "error-global", "Error in " + info + ": " + err);
 };
 const assets = [
-  '@/assets/images/logo.png',
-  '@/assets/images/home-area.png',
-  '@/assets/images/back-btn.png',
-  '@/assets/images/play-btn.png',
-  '@/assets/images/replay.png'
+  "@/assets/images/logo.png",
+  "@/assets/images/home-area.png",
+  "@/assets/images/back-btn.png",
+  "@/assets/images/play-btn.png",
+  "@/assets/images/replay.png",
 ];
 
-window.onload = function() {
-
+window.onload = function () {
   // When the window loads, start to initialize the SDK
-  FBInstant.initializeAsync().then(function() {
-
+  FBInstant.initializeAsync().then(function () {
     // We can start to load assets
     for (let i in assets) {
       // When preloading assets, make sure to report the progress
-      FBInstant.setLoadingProgress(i / assets.length * 100);
+      FBInstant.setLoadingProgress((i / assets.length) * 100);
     }
 
     // Now that assets are loaded, call startGameAsync
@@ -37,42 +36,42 @@ window.onload = function() {
 };
 
 function onStart() {
-  
   var userInfo = {
     id: FBInstant.player.getID(),
     name: FBInstant.player.getName(),
     info: FBInstant.player.getPhoto(),
     context: FBInstant.context.getType(),
-    entryPointData: FBInstant.getEntryPointData()
-  }
-  store.commit("UPDATE_INFO", userInfo)
-  store.commit("SET_I18N", FBInstant.getLocale())
+    entryPointData: FBInstant.getEntryPointData(),
+  };
+  store.commit("UPDATE_INFO", userInfo);
+  store.commit("SET_I18N", FBInstant.getLocale());
   FBInstant.player
-  .getDataAsync([
-    'soundSetting', 
-    'musicSetting',
-    'lastNLevel',
-    'lastPoints',
-    'gameType'
-  ])
-  .then(function(data) {
-    console.log(data)
-    
-    store.commit("SET_LASTNLEVEL", data['lastNLevel'])
-    store.commit("SET_LASTPOINTS", data['lastPoints'])
-    store.commit("SET_GAMETYPE", data['gameType'])
-    if (typeof data['gameType'] !== 'undefined' && data['gameType'].indexOf('sound') >= 0) {
-      store.commit("SET_SOUND_SETTING", true)
-    } else 
-      store.commit("SET_SOUND_SETTING", data['soundSetting'])
-    store.commit("SET_MUSIC_SETTING", data['musicSetting'])
-  });
+    .getDataAsync([
+      "soundSetting",
+      "musicSetting",
+      "lastNLevel",
+      "lastPoints",
+      "gameType",
+    ])
+    .then(function (data) {
+      console.log(data);
+
+      store.commit("SET_LASTNLEVEL", data["lastNLevel"]);
+      store.commit("SET_LASTPOINTS", data["lastPoints"]);
+      store.commit("SET_GAMETYPE", data["gameType"]);
+      if (
+        typeof data["gameType"] !== "undefined" &&
+        data["gameType"].indexOf("sound") >= 0
+      ) {
+        store.commit("SET_SOUND_SETTING", true);
+      } else store.commit("SET_SOUND_SETTING", data["soundSetting"]);
+      store.commit("SET_MUSIC_SETTING", data["musicSetting"]);
+    });
   // console.log(store.state.user.info)
   new Vue({
     router,
     store,
     i18n,
-    render: h => h(App)
-  }).$mount('#app')
+    render: (h) => h(App),
+  }).$mount("#app");
 }
-
